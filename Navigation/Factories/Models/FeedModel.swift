8b7691c479d53fc742b9632845815a3bd24b9ea6
alexey-305 <enot.in100g1@gmail.com>
2026-05-06 10:02:14 +0400

    feat: MVC - добавлен CustomButton, FeedModel, обновлён FeedViewController

--- Navigation/Factories/Models/FeedModel.swift
+++ Navigation/Factories/Models/FeedModel.swift
@@ -2,15 +2,12 @@ import Foundation
 
 class FeedModel {
     
-    // MARK: - Properties
     private let secretWord = "Swift"
     
-    // MARK: - Public
     var secretWordForDisplay: String {
         return secretWord
     }
     
-    // MARK: - Methods
     func check(word: String) -> Bool {
         return word.lowercased() == secretWord.lowercased()
     }
